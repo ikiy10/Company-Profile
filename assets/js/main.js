7cb9f426a7dd452e5e0a8599071707b43a78a73d
AriHyuk <coygalang990@gmail.com>
2024-05-28 15:15:13 +0700

    menyelesaikan carousel

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,13 +4,15 @@
             strings: ["Welcome to ArCe"],
             typeSpeed: 100, // Kecepatan ketik (ms)
             backSpeed: 50, // Kecepatan menghapus teks (ms)
-            loop: false // Tidak mengulang animasi
+            loop: false, // Tidak mengulang animasi
+            showCursor: false // Menghilangkan kursor kedip-kedip
         });
 
         let typed2 = new Typed('.typed-text-2', {
             strings: ["Aman, Cepat dan Terpercaya"],
             typeSpeed: 50, // Kecepatan ketik (ms)
             backSpeed: 50, // Kecepatan menghapus teks (ms)
-            loop: false // Tidak mengulang animasi
+            loop: false, // Tidak mengulang animasi
+            showCursor: false // Menghilangkan kursor kedip-kedip
         });
     });
